ccf76e2eaceee8c319fefe2c88067d9044ea41cb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-09 14:14:45 +0100

    ok

--- app/08-REPL/bot.js
+++ app/08-REPL/bot.js
@@ -3,11 +3,9 @@ const repl = require('repl');
 const replServer = repl.start('javascript> ');
 
 // when invoking the command, prefix it with dot (.): .sayhello 
-
 replServer.defineCommand('sayhello', {
 	help: 'Say hello',
 	action(name) {
-		this.clearBufferedCommand();
 		console.log(`Hello, ${name}!`);
 		this.displayPrompt();
 	}
