58ae8064d19d440b630de2455d1c8b057727a277
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-09 16:15:20 +0100

    console chalk

--- app/11-console/console.js
+++ app/11-console/console.js
@@ -1,5 +1,7 @@
 const myLog = require('./file-log.js');
 
+const chalk = require('chalk');
+
 console.clear();
 
 // print nothing, but start a timer 
@@ -7,6 +9,7 @@ console.time('my-app');
 
 console.log('coucou');
 console.log('here is an object', { coucou: 'hello', '#$%^': 123 });
+console.dir({ coucou: 'hello', '#$%^': 123 }, { colors: true });
 
 myLog.debug('I log in a file', { '1asd': 12345 });
 myLog.warn('Warning...', [1, 2, 'coucou']);
@@ -17,6 +20,8 @@ try {
 } catch (e) {
 	myLog.error('catched an error: ', e);
 }
+
+console.log(chalk.blue('Oh I am in blue ;O)'));
 
 // do nothing. (will not truncate the file like you could expect.)
 myLog.clear();
