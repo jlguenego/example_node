25a2cef6b3dadf629c359e43637cbbce43c78d13
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-11 18:55:18 +0100

    udp

---
+++ app/14-net/udpclient.js
@@ -0,0 +1,6 @@
+const dgram = require('dgram');
+const message = Buffer.from('Some bytes');
+const client = dgram.createSocket('udp4');
+client.send(message, 41234, 'localhost', (err) => {
+	client.close();
+});
