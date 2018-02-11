4bfcec7f30dc4dc8a48780384cc86e60e46f95df
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-11 18:50:48 +0100

    ipc

--- app/16-ipc/master.js
+++ app/16-ipc/master.js
@@ -1,19 +1,18 @@
 const fork = require('child_process').fork;
 const path = require('path');
+const os = require('os');
 const program = path.resolve('child.js');
 console.log('program', program);
 const child = fork(program, [], {
 	stdio: ['pipe', 'pipe', 'pipe', 'ipc']
 });
 
+console.log('cpus', os.cpus().length);
+
 child.on('message', message => {
 	console.log('message from child:', message);
 	child.send('Hi');
 });
-
-// child.stdout.on('data', data => {
-// 	console.log(`child.stdout: ${data}`);
-// });
 
 child.stdout.pipe(process.stdout);
 
