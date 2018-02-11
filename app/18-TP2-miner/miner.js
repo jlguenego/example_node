7d6b14e33d71af84b0f7fa16e66c1e84063cf3da
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-11 20:24:39 +0100

    TP minage

---
+++ app/18-TP2-miner/miner.js
@@ -0,0 +1,69 @@
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const crypto = require('crypto');
+const fs = require('fs');
+
+const zeroes = new Array(6 + 1).join('0');
+
+const workerNbr = numCPUs;
+
+const transaction = fs.readFileSync('transaction.input').toString();
+
+function mine(id) {
+	let nonce = id;
+	// eslint-disable-next-line
+	while (true) {
+		const hash = crypto.createHash('sha256');
+		hash.update(transaction + nonce);
+		const output = hash.digest('hex');
+		if (output.startsWith(zeroes)) {
+			const result = { output, nonce };
+			console.log('result', result);
+			return result;
+		}
+		nonce += workerNbr;
+	}
+}
+
+
+
+if (cluster.isMaster) {
+	console.log(`Master ${process.pid} is running`);
+
+	// Fork workers.
+	const workers = [];
+	for (let i = 0; i < workerNbr; i++) {
+		const worker = cluster.fork();
+		workers.push(worker);
+		worker.send(i);
+		worker.on('message', message => {
+            console.log('message from child:', message);
+            while (workers.length) {
+                const c = workers.pop();
+                console.log('killing', c.process.pid);
+                c.process.kill();
+            }
+		});
+	}
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`worker ${worker.process.pid} exit with code: ${code}`);
+    });
+    
+    process.on('exit', () => {
+        console.log(`mining took ${process.uptime()} seconds.`);
+    });
+} else {
+	// we are in a child.
+	process.on('message', id => {
+		console.log('message from parent:', id);
+		setTimeout(() => {
+			const output = mine(id);
+			if (process.send) {
+				process.send(JSON.stringify(output));
+			}
+		}, 1000);
+
+	});
+
+	console.log(`Worker ${process.pid} started`);
+}
