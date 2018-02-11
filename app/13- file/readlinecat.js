b7ed47db97f46357515680a8c5b41fedb754d939
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-11 15:45:31 +0100

    ok

--- app/13- file/readlinecat.js
+++ app/13- file/readlinecat.js
@@ -7,10 +7,10 @@ const files = process.argv.slice(2);
 files.forEach((file) => {
 	try {
 		let lineNbr = 0;
-        const stream = fs.createReadStream(path.resolve(__dirname, file));
-        stream.on('error', (...args) => {
-            console.log('Error: probably the file does not exist', args);
-        });
+		const stream = fs.createReadStream(path.resolve(__dirname, file));
+		stream.on('error', (...args) => {
+			console.log('Error: probably the file does not exist', args);
+		});
 		stream
 			.pipe(es.split('\n'))
 			.pipe(es.map(function(line, cb) {
@@ -19,22 +19,20 @@ files.forEach((file) => {
 				// work (synchronous in this example)
 				lineNbr++;
 				const nbr = `${lineNbr}`.padStart(3);
-				
-				setTimeout(() => {
-                    const parsedLine = `${nbr}:\t${line}\n`;
-                    
-                    // if you do this, you will not be sure to print in the right order.
-                    // process.stdout.write(parsedLine);
 
-                    // resume the readstream when you are finished to process the line.
+				setTimeout(() => {
+					const parsedLine = `${nbr}:\t${line}\n`;
+
+					// if you do this, you will not be sure to print in the right order.
+					// process.stdout.write(parsedLine);
+
+					// resume the readstream when you are finished to process the line.
 					stream.resume();
 					cb(null, parsedLine);
 				}, 1000 / lineNbr);
-            }))
-            .pipe(process.stdout);
+			}))
+			.pipe(process.stdout);
 	} catch (e) {
 		console.log('error', e);
 	}
 });
-
-
