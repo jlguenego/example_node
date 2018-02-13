ea8906be0566f9f6c1e1f514b6274b5d5e0703ee
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-13 09:25:11 +0100

    server http

--- app/22-http-route/http.js
+++ app/22-http-route/http.js
@@ -1,54 +1,47 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 9000;
 
 const server = http.createServer((req, res) => {
-	console.log('req.url', req.url);
-	console.log('req.method', req.method);
+	const { method, url, headers } = req;
 
-    const { method, url, headers } = req;
-    
-    console.log('req.headers', headers);
+	const userAgent = (headers['user-agent'].match(/Chrome/)) ? 'Chrome' : 'Not Chrome';
 
-    const userAgent = (headers['user-agent'].match(/Chrome/)) ? 'Chrome': 'Not Chrome';
-
-	if (method === 'GET') {
-		if (url.match(/\/hello.*/)) {
-            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-            setTimeout(() => {
-                res.end(`Your GET url starts with hello and take 2s. User agent: ${userAgent}`, 'utf8');
-            }, 2000);
-			
-			return;
-		}
-		if (url.match(/\/world.*/)) {
+	switch (method) {
+		case 'GET':
+			if (url.match(/\/hello.*/)) {
+				setTimeout(() => {
+					const content = fs.readFileSync('./hello.html');
+					res.end(content, 'utf8');
+				}, 500);
+			} else if (url.match(/\/world.*/)) {
+				const content = fs.readFileSync('./world.html');
+				res.end(content, 'utf8');
+			} else {
+                let content = fs.readFileSync('./index.html');
+                content = content.toString().replace(/\*\*\*USER_AGENT\*\*\*/g, userAgent);
+				res.end(content, 'utf8');
+			}
+			break;
+		case 'POST':
+			if (url.match(/\/hello.*/)) {
+				res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+				res.end('Your POST url starts with hello', 'utf8');
+				return;
+			} else if (url.match(/\/world.*/)) {
+				res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+				res.end('Your POST url starts with world', 'utf8');
+				return;
+			} else {
+				res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+				res.end('Your POST url is generic.', 'utf8');
+			}
+			break;
+		default:
 			res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-			res.end(`Your GET url starts with world. User agent: ${userAgent}`, 'utf8');
-			return;
-		}
-		res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-		res.end(`Your GET url is generic. User agent: ${userAgent}`, 'utf8');
-		return;
+			res.end('Your url is generic.', 'utf8');
 	}
-
-	if (method === 'POST') {
-		if (url.match(/\/titi.*/)) {
-			res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-			res.end('Your POST url starts with hello', 'utf8');
-			return;
-		}
-		if (url.match(/\/world.*/)) {
-			res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-			res.end('Your POST url starts with world', 'utf8');
-			return;
-		}
-		res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-		res.end('Your POST url is generic.', 'utf8');
-		return;
-	}
-	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-	res.end('Your url is generic.', 'utf8');
-
 });
 
 server.listen(port, () => {
