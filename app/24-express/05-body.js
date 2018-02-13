2a4c8ecbfe65e2dca591a2c2bbcbe3473b77f1b0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-13 16:59:44 +0100

    ok post

--- app/24-express/05-body.js
+++ app/24-express/05-body.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const serveIndex = require('serve-index');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
-
-
 // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
@@ -20,11 +20,22 @@ app.post('/ws/hello', (req, res, next) => {
 	res.json({ hello: req.body.name });
 });
 
+app.post('/ws/hello2', (req, res, next) => {
+	console.log('hello2 req.body', req.body);
+	if (!req.body.name) {
+		res.status(500).send('/ws/hello2 webservice failed: name missing.');
+		return;
+	}
+	let content = fs.readFileSync(path.resolve(__dirname, './hello2.html'));
+	content = content.toString().replace(/\*\*\*NAME\*\*\*/g, req.body.name);
+	res.send(content);
+});
+
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
 
 app.use((req, res) => {
-	console.log('req.url', req.url);	
+	console.log('req.url', req.url);
 	res.status(404).sendFile('404.html', { root: __dirname });
 });
 
