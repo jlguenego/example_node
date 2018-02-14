c316373e21e6e1ce933b9e70063949595d475457
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-14 15:09:13 +0100

    ok

--- app/27-Mongodb/create.js
+++ app/27-Mongodb/create.js
@@ -1,33 +1,27 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
+// Use connect method to connect to the server
+MongoClient.connect('mongodb://localhost:27017').then(client => {
+	console.log('Connected successfully to server');
+	client.close();
+	return client;
+}).then(e => {
+}).catch(e => {
+	console.error(e.message);
+	process.exit(1);
+});
 
 const insertDocuments = function(db, callback) {
 	// Get the documents collection
 	const collection = db.collection('documents');
 	// Insert some documents
 	collection.insertMany([
-		{ a: 1 }, { a: 2 }, { a: 3 }
+	  {a : 1}, {a : 2}, {a : 3}
 	], function(err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
-		console.log("Inserted 3 documents into the collection");
-		callback(result);
+	  assert.equal(err, null);
+	  assert.equal(3, result.result.n);
+	  assert.equal(3, result.ops.length);
+	  console.log("Inserted 3 documents into the collection");
+	  callback(result);
 	});
-}
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-	assert.equal(null, err);
-	console.log('Connected successfully to server');
-
-	const db = client.db(dbName);
-
-	client.close();
-});
+  }
--- app/27-Mongodb/test-connection.js
+++ app/27-Mongodb/test-connection.js
@@ -1,9 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
 
+let client;
+
 // Use connect method to connect to the server
-MongoClient.connect('mongodb://localhost:27017').then(client => {
+MongoClient.connect('mongodb://localhost:27017').then(c => {
 	console.log('Connected successfully to server');
+	client = c;
+}).then(() => {		
 	client.close();
+	console.log('Disconnected.');
 }).catch(e => {
 	console.error(e.message);
 	process.exit(1);
