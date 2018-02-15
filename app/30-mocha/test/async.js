0d59e3ddb4e130e5292897d359deac0484933536
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-02-15 08:39:31 +0100

    mocha

---
+++ app/30-mocha/test/async.js
@@ -0,0 +1,15 @@
+// const assert = require('assert');
+describe('Array', function() {
+	describe('#indexOf()', function() {
+		it('should wait at least 1000ms before executing.', function(done) {
+			const date = new Date().getTime();
+			setTimeout(() => {
+				const now = new Date().getTime();
+				const test = now - date >= 1000;
+				assert.ok(test);
+				done();
+			}, 1000);
+			
+		});
+	});
+});
