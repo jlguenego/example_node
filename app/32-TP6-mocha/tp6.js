60eb8c29cee2c40e124a83fc3e44d52c89497f84
dphengsiaroun <dphengsiaroun@outlook.fr>
2018-02-15 17:17:51 +0100

    TP6 working

--- app/32-TP6-mocha/tp6.js
+++ app/32-TP6-mocha/tp6.js
@@ -1,5 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const axios = require('axios');
+const async = require('async');
+const util = require('util');
+const asyncEach = util.promisify(async.each);
 
 async function resetDB() {
 	try {
@@ -18,12 +21,33 @@ async function resetDB() {
 }
 
 async function createUser(user) {
-    const response = await axios.post('http://localhost:8000/ws/users', user);
-    return response;
+	const response = await axios.post('http://localhost:8000/ws/users', user);
+	return response;
 }
 
 async function getDBState() {
+	try {
+		const client = await MongoClient.connect('mongodb://localhost:27017');
+		const db = client.db('TP5');
+        let list = await db.listCollections().toArray();
+		const result = {
+			TP5: {},
+		};
 
+		await asyncEach(list, async c => {
+			const collection = db.collection(c.name);
+			const array = await collection.find({}).toArray();
+			array.forEach(d => {
+				d._id = d._id.toString();
+				delete d.__v;
+			});
+			result.TP5[c.name] = array;
+        });
+		await client.close();
+		return result;
+	} catch (e) {
+		process.exit(1);
+	}
 }
 
 module.exports = {
